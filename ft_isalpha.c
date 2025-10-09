bea23839bb2693b45fcf9f9c99a84a9c3458bc04
Danila Zhukov <dzhukov@3-F-12.42heilbronn.de>
2025-10-09 20:46:08 +0200

    Add ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -5,22 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/28 19:05:51 by dzhukov           #+#    #+#             */
-/*   Updated: 2025/05/28 19:31:45 by dzhukov          ###   ########.fr       */
+/*   Created: 2025/10/09 20:05:39 by dzhukov           #+#    #+#             */
+/*   Updated: 2025/10/09 20:18:48 by dzhukov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha(int c)
+
+int	ft_isalpha(char c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	return (0);
 }
 
-int	main(void)
-{
-	printf("Res: %d\n", ft_isalpha(0));
-	return (0);
-}
+// int	main(void)
+// {
+// 	printf("Res: %d\n", ft_isalpha('A'));
+// 	return (0);
+// }
--- libft.h
+++ libft.h
@@ -8,6 +8,6 @@
 
 
 
-int	ft_isalpha(int c);
+int	ft_isalpha(char c);
 
 #endif
