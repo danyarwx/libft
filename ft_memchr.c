18e0df4fe10b7adf699a4caa43933b30abe649d4
Danila Zhukov <dzhukov@3-H-2.42heilbronn.de>
2025-10-14 18:39:33 +0200

    Add till memchr.

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 18:23:11 by dzhukov           #+#    #+#             */
+/*   Updated: 2025/10/14 18:34:07 by dzhukov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr[i] == (unsigned char *)c)
+			return ((void *)&ptr[i]);
+		i++;
+	}
+	return (NULL);
+}
---
+++ ft_strncmp.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 17:57:50 by dzhukov           #+#    #+#             */
+/*   Updated: 2025/10/14 18:21:38 by dzhukov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	char	str[] = "you";
+// 	char	str2[] = "you";
+
+// 	printf("The result is : %d.", ft_strncmp(str, str2, 7));
+// 	return (0);
+// }
