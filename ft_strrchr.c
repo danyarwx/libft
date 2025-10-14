2eb148e5e7eeeda06fd89957e889f3e8fb9689cb
Danila Zhukov <dzhukov@3-H-2.42heilbronn.de>
2025-10-14 17:57:06 +0200

    Add strchr and strrchr.

---
+++ ft_strrchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 16:33:16 by dzhukov           #+#    #+#             */
+/*   Updated: 2025/10/14 17:55:37 by dzhukov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*last;
+	size_t	i;
+
+	last = NULL;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+		{
+			last = s + i;
+		}
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	return (last);
+}
