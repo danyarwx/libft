33cdd7f4ccdf729aca77b193c84361e9f370c761
Danila Zhukov <dzhukov@3-H-2.42heilbronn.de>
2025-10-14 14:40:03 +0200

    Complete strlcpy.

---
+++ ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 13:25:47 by dzhukov           #+#    #+#             */
+/*   Updated: 2025/10/14 14:37:18 by dzhukov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	len_s;
+
+	len_s = ft_strlen(src);
+	if (size > len_s + 1)
+	{
+		ft_memmove(dst, src, len_s + 1);
+		dst[len_s] = '\0';
+	}
+	else if (size != 0)
+	{
+		ft_memmove(dst, src, size - 1);
+		dst[size - 1] = '\0';
+	}
+	return (len_s);
+}
