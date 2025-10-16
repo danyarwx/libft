bd5848ccc49774a1824fdb3d7be76e28956f858f
Danila Zhukov <dzhukov@3-G-10.42heilbronn.de>
2025-10-16 19:23:47 +0200

    Add substr and strjoin.

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,36 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/15 16:47:52 by dzhukov           #+#    #+#             */
-/*   Updated: 2025/10/16 19:10:50 by dzhukov          ###   ########.fr       */
+/*   Created: 2025/10/16 19:04:19 by dzhukov           #+#    #+#             */
+/*   Updated: 2025/10/16 19:21:54 by dzhukov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	n;
+	size_t	len1;
+	size_t	len2;
 	size_t	i;
 	char	*ptr;
 
-	n = ft_strlen(s);
-	if (start >= n || !s)
+	if (!s1 || !s2)
 		return (NULL);
-	n += start;
-	if (len > n)
-		len = n;
-	ptr = (char *)ft_calloc(len + 1, sizeof(char));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	ptr = (char *)ft_calloc(len1 + len2 + 1, sizeof(char));
 	if (!ptr)
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (s1[i])
+		ptr[i++] = s1[i++];
+	i = 0;
+	while (s2[i])
 	{
-		ptr[i] = s[start + i];
+		ptr[i + len1] = s2[i];
 		i++;
 	}
 	ptr[i] = '\0';
