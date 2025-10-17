2ea13b1522e17a2005da12ef3aad322ec7f0d9ed
Danila Zhukov <dzhukov@3-F-12.42heilbronn.de>
2025-10-17 17:45:16 +0200

    Add itoa (reverse the string left).

---
+++ ft_itoa.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 17:35:45 by dzhukov           #+#    #+#             */
+/*   Updated: 2025/10/17 17:44:46 by dzhukov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	int	i;
+	int	count;
+	char *str;
+	char c;
+
+
+	count = 0;
+	while (n > 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	str = (char *)ft_calloc(count + 1, sizeof(char));
+	i = 0;
+	while(n > 0)
+	{
+		str[i++] = n % 10 + '0';
+		n = n / 10;
+	}
+	//reverse string
+
+
+}
