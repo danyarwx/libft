beee1e68716932f7cd6cc9909d85ac22571ccfde
Danila Zhukov <dzhukov@3-F-12.42heilbronn.de>
2025-10-17 10:51:04 +0200

    Check strtrim in the future.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,13 @@
 /*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 19:24:33 by dzhukov           #+#    #+#             */
-/*   Updated: 2025/10/16 19:56:56 by dzhukov          ###   ########.fr       */
+/*   Updated: 2025/10/17 10:50:30 by dzhukov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+//TO CHECK!!!
 
 int	ft_isset(char c, char const *set)
 {
