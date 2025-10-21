2732ba4c2d7501c7b59a0d19c726ee11143c479c
Danila Zhukov <dzhukov@2-C-3.42heilbronn.de>
2025-10-21 18:41:59 +0200

    Add ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 18:11:34 by dzhukov           #+#    #+#             */
+/*   Updated: 2025/10/21 18:41:30 by dzhukov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	size_t	len;
+	char	*str;
+
+	len = ft_strlen(s);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	i = 0;
+	while (i < len)
+		str[i] = f(i, s[i++]);
+	return (str);
+}
