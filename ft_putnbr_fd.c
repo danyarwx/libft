814b6b06d13ed30cc3ef08f9af16c132ebd2eb0c
Danila Zhukov <dzhukov@3-G-6.42heilbronn.de>
2025-10-23 13:15:23 +0200

    Rewrite ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 19:05:55 by dzhukov           #+#    #+#             */
-/*   Updated: 2025/10/22 15:38:03 by dzhukov          ###   ########.fr       */
+/*   Updated: 2025/10/23 13:13:29 by dzhukov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,30 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
+	char	c;
 
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
-	free(str);
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = n * (-1);
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+	}
+	c = (n % 10) + '0';
+	write(fd, &c, 1);
 }
+
+// int	main(void)
+// {
+// 	ft_putnbr_fd(2147483647, 1);
+// 	write(1, "\n", 1);
+// 	ft_putnbr_fd(-2147483648, 1);
+// 	return (0);
+// }
