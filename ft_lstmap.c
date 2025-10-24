28daf87f59bff803014cffdb3317af2d582024ec
Danila Zhukov <dzhukov@2-D-6.42heilbronn.de>
2025-10-24 18:57:50 +0200

    Finish the bonus part

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,25 +6,30 @@
 /*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 12:09:37 by dzhukov           #+#    #+#             */
-/*   Updated: 2025/10/24 12:15:42 by dzhukov          ###   ########.fr       */
+/*   Updated: 2025/10/24 18:54:12 by dzhukov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlst;
 	t_list	*tmp;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	tmp = lst;
-	while (tmp)
+	newlst = NULL;
+	while (lst)
 	{
-		newlst = (t_list *)malloc(sizeof(t_list));
-		if (!newlst)
-			//free function
-		newlst->content = f(tmp->content);
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&newlst, tmp);
+		lst = lst->next;
 	}
+	return (newlst);
 }
